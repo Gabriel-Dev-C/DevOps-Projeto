d7a8a8cc18087299646fafda8a2fab7937d35e91
Gabriel <gacmartins@gmail.com>
2025-05-08 10:09:26 -0300

    cypress-table-auto

--- cypress/e2e/filtro-cursos.cy.js
+++ cypress/e2e/filtro-cursos.cy.js
@@ -3,9 +3,17 @@ describe('Navegando na tabela de cursos', () => {
         cy.visit('http://127.0.0.1:5500/src/index/cursos/cursos.html')
     })
 
-    it ('Filtrar cursos por nome', () => {
-        // Verifica se a tabela possui 3 linhas inicialmente
-        cy.get('input[type=search]').type('Ciências da computação')
-        cy.get('table tbody tr').should('have.length', 1)
+    it('Ordenar cursos por ordem alfabética', () => {
+        // Clica no cabeçalho da coluna "Curso" para ordenar
+        cy.get('table thead th').contains('Curso').click()
+
+        // Captura os textos das células da coluna "Curso"
+        cy.get('table tbody tr td:first-child').then(($cells) => {
+            const cursos = [...$cells].map(cell => cell.innerText)
+
+            // Verifica se os cursos estão em ordem alfabética
+            const cursosOrdenados = [...cursos].sort((a, b) => a.localeCompare(b))
+            expect(cursos).to.deep.equal(cursosOrdenados)
+        })
     })
 })
